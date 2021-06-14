b75b8de963d0284749a73ee53fab011a28472732
ehedei <ehedeihg@gmail.com>
2021-06-14 14:29:59 +0100

    New Models

--- api/models/pet.model.js
+++ api/models/pet.model.js
@@ -1,5 +1,38 @@
 const mongoose = require('mongoose')
 
-const petSchema = new mongoose.Schema({})
+const petSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required']
+  },
+  birthdate: {
+    type: Date,
+    required: [true, 'Birthdate is required']
+  },
+  species: {
+    type: String,
+    required: [true, 'Species is required']
+  },
+  breed: String,
+  genre: {
+    type: String,
+    required: [true, 'Genre is required']
+  },
+  alive: {
+    type: Boolean,
+    required: [true, 'Status is required'],
+    default: true
+  },
+  description: String,
+  alergies: [String],
+  record: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'case'
+  }],
+  notes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'note'
+  }]
+})
 
 exports.PetModel = mongoose.model('pet', petSchema)
---
+++ api/models/vitalsigns.model.js
@@ -0,0 +1,9 @@
+const mongoose = require.mongoose
+
+exports.vitalSignsSchema = new mongoose.Schema({
+  weight: Number,
+  height: Number,
+  temperature: Number,
+  heartRate: Number,
+  breathingRate: Number
+})
